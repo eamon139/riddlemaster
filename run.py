6ca7901c7cf0a068a1f322db29d577ba7abd0b34
Eamon Kenny <eamokenny@gmail.com>
2018-08-13 17:29:59 +0000

    add frame work

---
+++ run.py
@@ -0,0 +1,53 @@
+import os
+import json
+from datetime import datetime
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+app.secret_key = 'some_secret'
+data = []
+
+
+def write_to_file(filename, data):
+    """Handle process of writing data to a file"""
+    with open(filename, "a") as file:
+        file.writelines(data)
+
+def add_messages(username, message):
+    """Add messages to the `messages` text file"""
+    write_to_file("data/messages.txt" , "({0}) {1} - {2}\n".format(
+            datetime.now().strftime("%H:%M:%S"),
+            username.title(),
+            message))
+    
+
+
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    """Main page ad user name ---------------------------------------"""
+    
+    # Handel the post request
+    if request.method == "POST":
+        write_to_file("data/users.txt", request.form["username"] + "\n")
+        return redirect(request.form["username"])
+    return render_template("index.html")
+
+
+
+
+
+@app.route('/<username>')
+def user(username):
+    """Display welcome MGS messages-----------------------------------"""
+    return render_template("thegame.html", 
+                            username=username,)
+                            
+
+
+
+
+
+app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
+
